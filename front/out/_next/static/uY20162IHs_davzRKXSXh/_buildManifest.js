6c71c9805349253fa4ca29c774c5ca5e9d8c1c23
Kostya Shkryob <kshkryob@gmail.com>
2021-12-25 19:20:52 +0200

    Add compiled front

---
+++ front/out/_next/static/uY20162IHs_davzRKXSXh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/793-c463f0d227d0cbc9.js","static/chunks/pages/index-669248bd1bee5b9b.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
